ea4d769b4f21905a50311174b28a024234a3d6b2
YuriMikoto <theblackwolf13@hotmail.co.uk>
2017-05-30 15:01:36 +0100

    Trap floor added; note lighting/gravity.

---
+++ VRSnakesAndLadders-master/SnLVR/Assets/LadderClimb.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class LadderClimb : MonoBehaviour {
+
+    public static LadderClimb ladder;
+    public bool canUse;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.SetActive(false);
+        ladder = this;
+
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            GameObject.Find("Player").GetComponent<Movement>().moveUp = true;
+        }
+    }
+
+     void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject.Find("Player").GetComponent<Movement>().moveUp = true;
+        }
+    }
+}
---
+++ VRSnakesAndLadders-master/SnLVR/Assets/Movement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    // How fast to move
+    public float speed = 3.0F;
+    // Should I move forward or not
+    public bool moveForward;
+    // CharacterController script
+    private CharacterController controller;
+    // GvrViewer Script
+    private GvrEditorEmulator gvrViewer;
+    // VR Head
+    private Transform vrHead;
+
+    public static Movement playerMove;
+
+
+    public bool canMoveOnClick;
+
+    public bool moveUp;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Find the CharacterController
+        controller = GetComponent<CharacterController>();
+        // Find the GvrViewer on child 0
+        gvrViewer = transform.GetChild(0).GetComponent<GvrEditorEmulator>();
+        // Fnd the VR Head
+        vrHead = Camera.main.transform;
+
+        playerMove = this;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // In the Google VR button press
+        if (Input.GetButtonDown("Fire1") && canMoveOnClick)
+        {
+            // Change the state of moveForward
+            moveForward = !moveForward;
+        }
+
+        // Check to see if I should move
+        if (moveForward && !moveUp)
+        {
+            // Find the forward direction
+            Vector3 forward = vrHead.TransformDirection(Vector3.forward);
+            // Tell CharacterController to move forward
+            controller.SimpleMove(forward * speed);
+        }
+
+        if(moveUp && vrHead.eulerAngles.x < 320  && moveForward)
+        {
+            controller.Move(Vector3.up * speed * Time.deltaTime);
+        }
+    }
+
+}
