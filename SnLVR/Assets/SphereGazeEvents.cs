67dcb5edb58a3c6b002f99456b087ed447c4573b
C00148197@itcarlow.ie <C00148197@itcarlow.ie>
2017-06-02 16:25:44 +0100

    Gaze commit

---
+++ SnLVR/Assets/SphereGazeEvents.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereGazeEvents : MonoBehaviour
+{
+
+    // This event is going to be fired when we look at the sphere
+    public void OnGazeEnter()
+    {
+        Debug.Log("Gaze entered");
+    }
+
+    // This event is going to be fired when we stop looking at the sphere
+    public void OnGazeLeave()
+    {
+        Debug.Log("Gaze left");
+    }
+}
